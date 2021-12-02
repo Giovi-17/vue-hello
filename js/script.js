9a99cd069e4f4a87476e3b8890484709926de3ef
Giovi-17 <giovanni.nuzzo17@gmail.com>
2021-12-02 13:18:34 +0100

    Bonus

--- js/script.js
+++ js/script.js
@@ -5,7 +5,8 @@ const app = new Vue(
 
         el: "#root",
         data: {
-            textHTML: "Sono un h1 e il testo proviene dal javascript"
+            textHTML: "Sono un h1 e il testo proviene dal javascript",
+            linkImage : "https://miro.medium.com/max/1024/1*uywKrvOm4CKZTI6v3a9TjA.jpeg"
         }
 
     }
